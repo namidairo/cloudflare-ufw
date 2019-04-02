894ff3061941e06b7ba532aa70466b731da073ea
Richard Huynh <1764089+namidairo@users.noreply.github.com>
2019-04-02 15:58:38 +1100

    Update Nginx logging config too
    If you're MITM'ing between my server and Cloudflare, chances are I have bigger problems.

--- cloudflare-ufw.sh
+++ cloudflare-ufw.sh
@@ -12,6 +12,19 @@ for cfip in `cat ips-v6`; do ufw allow from $cfip; done
 
 ufw reload > /dev/null
 
+# Nginx
+
+# Empty out old config
+truncate -s 0 /etc/nginx/conf.d/cloudflare.conf
+
+# Append to config
+for cfip in `cat ips-v4`; do echo "set_real_ip_from $cfip;" >> /etc/nginx/conf.d/cloudflare.conf; done
+for cfip in `cat ips-v6`; do echo "set_real_ip_from $cfip;" >> /etc/nginx/conf.d/cloudflare.conf; done
+echo "real_ip_header CF-Connecting-IP;" >> /etc/nginx/conf.d/cloudflare.conf;
+
+# Reload nginx
+service nginx reload
+
 # OTHER EXAMPLE RULES
 # Examples to retrict to port 80
 #for cfip in `cat ips-v4`; do ufw allow from $cfip to any port 80 proto tcp; done
